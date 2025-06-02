f7eb5248aa1106a919aef6a59ced9ebcdf6fe2f8
Ali Bardide <alibardide5124@gmail.com>
2025-06-02 14:04:05 +0330

    Fix product image scale in DetailsScreen

--- app/src/main/java/com/phoenix/bit_cart/screen/details/DetailsScreen.kt
+++ app/src/main/java/com/phoenix/bit_cart/screen/details/DetailsScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -114,7 +115,8 @@ fun DetailsScreen(
                         product!!.imageUrl?.get(0).let {
                             AsyncImage(
                                 model = it,
-                                contentDescription = null
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth
                             )
                         }
                         Box(
