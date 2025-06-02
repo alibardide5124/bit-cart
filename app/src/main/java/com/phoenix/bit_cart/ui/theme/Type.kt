2cb4585391c23d4cbaed36b7e7d6d52f2603aa0c
Ali Bardide <alibardide5124@gmail.com>
2025-06-02 10:06:54 +0330

    Add Oxanium font

--- app/src/main/java/com/phoenix/bit_cart/ui/theme/Type.kt
+++ app/src/main/java/com/phoenix/bit_cart/ui/theme/Type.kt
@@ -2,9 +2,19 @@ package com.phoenix.bit_cart.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.phoenix.bit_cart.R
+
+
+val Oxanium = FontFamily(
+    Font(R.font.oxanium_regular, FontWeight.Normal),
+    Font(R.font.oxanium_medium, FontWeight.Medium),
+    Font(R.font.oxanium_semibold, FontWeight.SemiBold),
+    Font(R.font.oxanium_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +41,24 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
-)
+).defaultFontFamily(Oxanium)
+
+fun Typography.defaultFontFamily(fontFamily: FontFamily): Typography {
+    return this.copy(
+        displayLarge = this.displayLarge.copy(fontFamily = fontFamily),
+        displayMedium = this.displayMedium.copy(fontFamily = fontFamily),
+        displaySmall = this.displaySmall.copy(fontFamily = fontFamily),
+        headlineLarge = this.headlineLarge.copy(fontFamily = fontFamily),
+        headlineMedium = this.headlineMedium.copy(fontFamily = fontFamily),
+        headlineSmall = this.headlineSmall.copy(fontFamily = fontFamily),
+        titleLarge = this.titleLarge.copy(fontFamily = fontFamily),
+        titleMedium = this.titleMedium.copy(fontFamily = fontFamily),
+        titleSmall = this.titleSmall.copy(fontFamily = fontFamily),
+        bodyLarge = this.bodyLarge.copy(fontFamily = fontFamily),
+        bodyMedium = this.bodyMedium.copy(fontFamily = fontFamily),
+        bodySmall = this.bodySmall.copy(fontFamily = fontFamily),
+        labelLarge = this.labelLarge.copy(fontFamily = fontFamily),
+        labelMedium = this.labelMedium.copy(fontFamily = fontFamily),
+        labelSmall = this.labelSmall.copy(fontFamily = fontFamily)
+    )
+}
