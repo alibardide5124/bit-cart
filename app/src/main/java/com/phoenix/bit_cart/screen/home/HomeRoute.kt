146782b10db8291daa1bfd18c4846d2439081a23
Ali Bardide <alibardide5124@gmail.com>
2025-06-04 15:56:00 +0330

    Add NavigationDrawer to Home Screen

--- app/src/main/java/com/phoenix/bit_cart/screen/home/HomeRoute.kt
+++ app/src/main/java/com/phoenix/bit_cart/screen/home/HomeRoute.kt
@@ -1,10 +1,32 @@
 package com.phoenix.bit_cart.screen.home
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.phoenix.bit_cart.data.model.Product
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeRoute(
@@ -14,12 +36,61 @@ fun HomeRoute(
 ) {
     val uiState by homeViewModel.uiState.collectAsStateWithLifecycle()
     val products by homeViewModel.products.collectAsStateWithLifecycle()
+    val coroutineScope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-    HomeScreen(
-        isLoading = uiState.isLoading,
-        products = products,
-        onTryAgain = { homeViewModel.onEvent(HomeUiEvent.Refresh) },
-        onClickAbout = { navigateToAbout() },
-        onClickProduct = { navigateToDetails(it) }
-    )
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = Modifier.fillMaxWidth(.8f)
+            ) {
+                Text(
+                    text = "Bit-Cart",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Spacer(Modifier.height(16.dp))
+                HorizontalDivider()
+                Spacer(Modifier.height(16.dp))
+                NavigationDrawerItem(
+                    label = { Text("Profile") },
+                    selected = false,
+                    onClick = {},
+                    icon = {
+                        Icon(imageVector = Icons.Outlined.Person, contentDescription = null)
+                    }
+                )
+                NavigationDrawerItem(
+                    label = { Text("Cart") },
+                    selected = false,
+                    onClick = {},
+                    icon = {
+                        Icon(imageVector = Icons.Outlined.ShoppingCart, contentDescription = null)
+                    }
+                )
+                Spacer(Modifier.height(16.dp))
+                HorizontalDivider()
+                Spacer(Modifier.height(16.dp))
+                NavigationDrawerItem(
+                    label = { Text("About") },
+                    selected = false,
+                    onClick = {
+                        navigateToAbout()
+                        coroutineScope.launch { drawerState.close() }
+                    },
+                    icon = { Icon(imageVector = Icons.Default.Info, contentDescription = null) }
+                )
+            }
+        }
+    ) {
+        HomeScreen(
+            isLoading = uiState.isLoading,
+            products = products,
+            onTryAgain = { homeViewModel.onEvent(HomeUiEvent.Refresh) },
+            onClickDrawerMenu = { coroutineScope.launch { drawerState.open() } },
+            onClickProduct = { navigateToDetails(it) }
+        )
+    }
 }
--- app/src/main/java/com/phoenix/bit_cart/screen/home/HomeScreen.kt
+++ app/src/main/java/com/phoenix/bit_cart/screen/home/HomeScreen.kt
@@ -4,20 +4,17 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -42,41 +39,40 @@ fun HomeScreen(
     isLoading: LoadingStatus,
     products: List<Product>,
     onTryAgain: () -> Unit,
-    onClickAbout: () -> Unit,
+    onClickDrawerMenu: () -> Unit,
     onClickProduct: (Product) -> Unit
 ) {
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text(stringResource(id = R.string.app_name))},
+                title = { Text(stringResource(id = R.string.app_name)) },
                 navigationIcon = {
-                    Row {
-                        Icon(
-                            imageVector = Icons.Default.Info,
-                            contentDescription = "About me",
-                            modifier = Modifier
-                                .clip(RoundedCornerShape(8.dp))
-                                .clickable { onClickAbout() }
-                                .padding(8.dp)
-                        )
-                        Spacer(Modifier.width(8.dp))
-                        Icon(
-                            imageVector = Icons.Outlined.ShoppingCart,
-                            contentDescription = "Cart"
-                        )
-                    }
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "Drawer",
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(8.dp))
+                            .clickable { onClickDrawerMenu() }
+                            .padding(8.dp)
+                    )
                 },
                 actions = {
                     Icon(
                         imageVector = Icons.Outlined.Search,
-                        contentDescription = "Search"
+                        contentDescription = "Search",
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(8.dp))
+                            .clickable { }
+                            .padding(8.dp)
                     )
                 }
             )
         }
     ) { innerPadding ->
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
-            when(isLoading) {
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)) {
+            when (isLoading) {
                 LoadingStatus.LOADING ->
                     item {
                         Column(
@@ -92,6 +88,7 @@ fun HomeScreen(
                             )
                         }
                     }
+
                 LoadingStatus.DONE -> {
                     if (products.isEmpty()) {
                         item {
@@ -115,6 +112,7 @@ fun HomeScreen(
                         )
                     }
                 }
+
                 LoadingStatus.FAILED -> {
                     item {
                         Column(
